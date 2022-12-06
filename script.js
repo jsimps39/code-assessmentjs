ce8aff9413c3a260dcca9e71c2b512e0ccbf8c57
James S <jamests1000@gmail.com>
2022-12-06 16:08:07 -0500

    fixed screen changing issue

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 var questionEl = document.querySelector(".question");
+var quizContainer = document.querySelector("#quizContainer")
+var buttonContainer = document.querySelector('#buttonContainer')
+var startButton = document.querySelector('#startButton');
+var quizScreen = document.querySelector('#quizScreen');
+var startScreen = document.querySelector('#startScreen');
+var endScreen = document.querySelector('#endScreen');
+var highScoreScreen = document.querySelector('#highScoreScreen');
+var scoreLabel = document.querySelector('#scoreLabel');
+var initialsInput = document.querySelector('#initialsInput');
+var submitScore = document.querySelector('#submitScore');
+var playAgain = document.querySelector('#playAgain');
+//var timeEl = document.querySelector('#time')
+
+var index = 0;
+var secondsLeft = 100;
+var timerInterval;
 var cursor = 0;
-var startButtonEl = document.getElementById('start');
 
 var questions = [
-    { text : "How much wood could a woodchuck chuck?",
+    { message : "How much wood could a woodchuck chuck?",
     correctAnswer: "b",
     possible: [
         "a. 10",
@@ -13,7 +28,7 @@ var questions = [
     ],
 },
     {
-        text: "did you have your break today?",
+        message: "did you have your break today?",
         correctAnswer: "a",
         possible: [
             "a. Yes",
@@ -21,7 +36,7 @@ var questions = [
         ],
 },
     {
-        text: "Do you like ice cream",
+        message: "Do you like ice cream",
         correctAnswer: "a",
         possible: [
             "a. Yes",
@@ -29,7 +44,7 @@ var questions = [
         ],
 },
     {
-        text: "What's your favorite pizza topping?",
+        message: "What's your favorite pizza topping?",
         correctAnswer: "a",
         possible: [
             "a. Pepperoni",
@@ -37,19 +52,101 @@ var questions = [
         ],
 }
  ];
+
+// var startTimer = function() {
+//     console.log('timer started')
+//     timerInterval = setInterval(function ()){
+//         secondsLeft --;
+
+//     }
+
+// }
+
+var moveForward = function(){
+    var correct = true;
+    index++;
+
+    if(index >= questions.length){
+        displayEndScreen();
+        return;
+    }
+
+    if(correct) {
+        console.log("right answer");
+    }else {
+        console.log("incorrect");
+        console.log("subtract time");
+    }
+    displayQuestion();
+}
+
+var hideAllScreens = function(){
+    var sections = document.querySelectorAll("section");
+    console.log(sections);
+    for(var i= 0; i < sections.length; i++){
+        sections[i].style.display="none"
+    }
+}
+
+startButton.addEventListener('click', function() {
+    //startTimer();
+    displayQuestion();
+    var sections = document.querySelectorAll("section");
+    for (var i =0; i< sections.length; i++){
+        sections[i].style.display = 'none';
+    }
+    quizScreen.style.display = "block";
+});
+
+buttonContainer.addEventListener('click', function(event){
+    var element = event.target;
+
+    if(element.matches('button')){
+        moveForward();
+    }
+})
+
+
 var correctAnswers = ["b", "a", "a", "a"] //numbers are index positions of 
 //correct answers in html file
 
+var displayEndScreen = function() {
+    console.log('display end screen');
+    var highScore = {
+        score: secondsLeft,
+        initials: initialsInput.value.trim(),
+    }
+    var highScoresList = JSON.parse(localStorage.getItem("highScores")) || [];
+    highScoresList.push(highScore);
+    localStorage.setItem("highScores", JSON.stringify(highScoresList));
 
+    highScoresList.sort(function (a,b)  {
+        a.score < b.score 
+    });
+
+    for(var i=0; i < highScoresList.length; i++){
+        var listItem = document.createElement('li');
+        listItem.textContent = highScoresList[i].initilas + ":" + highScoresList[i].score;
+        highScoreScreen.querySelector('ul').appendChild(listItem);
+    }
+    hideAllScreens();
+    endScreen.style.display = 'block';
+    scoreLabel.textContent = secondsLeft;
+    clearInterval(timerInterval);
+}
 
 var displayQuestion = function () {
-    questionEl.querySelector('h2').textContent = questions[cursor].text;
-    questionEl.querySelector('#possible').innerHTML = null;
-    for (var buttonLabel of questions[cursor].possible){
-        var buttonEl = document.createElement('button');
-        buttonEl.textContent = buttonLabel;
-        buttonEl.dataset.choice = buttonLabel[0];
-        questionEl.querySelector('#possible').appendChild(buttonEl);
+    var currentQuestion = questions[index];
+
+    quizScreen.querySelector('h2').textContent = currentQuestion.message;
+    quizScreen.querySelector('#buttonContainer').innerHTML = null;
+
+    //for (var buttonLabel of questions[index].possible){ //changed from cursor to index
+      for (var i = 0; i <currentQuestion.possible.length; i++){
+         var buttonEl = document.createElement('button');
+        buttonEl.textContent = currentQuestion.possible[i];
+        buttonEl.dataset.id = i; //buttonLabel[0];
+        quizScreen.querySelector('#buttonContainer').appendChild(buttonEl);
     }
 };
 
@@ -77,13 +174,20 @@ var start = function (event) {
 }
 
 //document.addEventListener('click', start)
-document.addEventListener('click', advance);
-// function saveScore() {
-//     var playerScore = {
-//         score: answer.value,
-//         timeLeft: time.value
-//     };
-// }
+submitScore.addEventListener('click', function(){
+    console.log('send to local storage');
+    hideAllScreens();
+    highScoreScreen.style.display = "block";
+});
+
+playAgain.addEventListener('click', function(){
+    console.log('display start screen');
+    hideAllScreens();
+    startScreen.style.display = "block";
+});
 
 //set up different start screen in a different container
-displayQuestion();
+hideAllScreens();
+startScreen.style.display = "block";
+//displayQuestion();
+//timeEl.textContent = secondsLeft;
