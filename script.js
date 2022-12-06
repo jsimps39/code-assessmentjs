5e8d7ee588fff930ed6261e5c7bd95f330687f07
James S <jamests1000@gmail.com>
2022-12-05 22:48:52 -0500

    added timer js file

--- script.js
+++ script.js
@@ -1,40 +1,89 @@
-var nextEl = document.querySelector('#next');
-var questions = document.querySelectorAll(".question");
+var questionEl = document.querySelector(".question");
 var cursor = 0;
+var startButtonEl = document.getElementById('start');
 
-// var questions = [
-//     "How much wood could a woodchuck chuck?",
-//     "did you have your break today?",
-//     "Do you like ice cream",
-//     "What's your favorite pizza topping?"
-// ];
-var correctAnswers = ["1", "1", "0", "1"] //numbers are index positions in html file
-
-var displayQuestion = function() {
-   for (var question of questions){
-    console.log(question);
-    if (question.dataset.index != cursor){
-        question.style.display = "none";
-    }else {
-        question.style.display = "block";
-    }
-    // return question; 
-   }
-    //nextEl.textContent = questions[cursor];
-};
-
-var advance = function(event) {
-    var element = event.target;
-    if (element.matches('.question button ')){
-    if(cursor < questions.length - 1){
-       var answer = element.dataset.choice === correctAnswers[cursor];
-        console.log(answer);
-       cursor++;
-    }
-    displayQuestion();
+var questions = [
+    { text : "How much wood could a woodchuck chuck?",
+    correctAnswer: "b",
+    possible: [
+        "a. 10",
+        "b. 20",
+        "c. 30",
+        "d. 40",
+    ],
+},
+    {
+        text: "did you have your break today?",
+        correctAnswer: "a",
+        possible: [
+            "a. Yes",
+            "b. No",
+        ],
+},
+    {
+        text: "Do you like ice cream",
+        correctAnswer: "a",
+        possible: [
+            "a. Yes",
+            "b. No",
+        ],
+},
+    {
+        text: "What's your favorite pizza topping?",
+        correctAnswer: "a",
+        possible: [
+            "a. Pepperoni",
+            "b. Sausage",
+        ],
 }
+ ];
+var correctAnswers = ["b", "a", "a", "a"] //numbers are index positions of 
+//correct answers in html file
+
+
+
+var displayQuestion = function () {
+    questionEl.querySelector('h2').textContent = questions[cursor].text;
+    questionEl.querySelector('#possible').innerHTML = null;
+    for (var buttonLabel of questions[cursor].possible){
+        var buttonEl = document.createElement('button');
+        buttonEl.textContent = buttonLabel;
+        buttonEl.dataset.choice = buttonLabel[0];
+        questionEl.querySelector('#possible').appendChild(buttonEl);
+    }
 };
 
-//nextEl.addEventListener('click', advance);
+var advance = function (event) {
+    var element = event.target;
+    if (element.matches('.question button ')) {
+        var answer = element.dataset.choice === correctAnswers[cursor];
+        console.log(element.dataset.choice);
+        console.log(correctAnswers[cursor]);
+        console.log(answer);
+        if (cursor < questions.length - 1) {
+            cursor++;
+            questionEl.dataset.index = cursor;
+        }  //else if cursor = questions.length - display high score page?
+        displayQuestion();
+    }
+};
+
+var start = function (event) {
+    var element = event.target;
+    if (element.matches('.start button')){
+        console.log("begin");
+        displayQuestion;
+    }
+}
+
+//document.addEventListener('click', start)
 document.addEventListener('click', advance);
-displayQuestion();
+// function saveScore() {
+//     var playerScore = {
+//         score: answer.value,
+//         timeLeft: time.value
+//     };
+// }
+
+//set up different start screen in a different container
+//displayQuestion();
---
+++ timer.js
@@ -0,0 +1,39 @@
+var timeEl = document.querySelector(".time");
+
+// Selects element by id
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 100;
+
+function displayTime(){
+  var label = "seconds";
+  if(secondsLeft === 1){
+    label = "second"
+  }
+  timeEl.textContent = secondsLeft + "  " + label + " left.";
+}
+
+function setTime() {
+  displayTime();
+  timeEl.textContent = secondsLeft + " seconds left.";
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds.";
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+// Function to create and append colorsplosion image
+function sendMessage() {
+  timeEl.textContent = "Times Up! ";
+}
+
+setTime();
